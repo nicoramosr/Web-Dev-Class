e33905bf3609497a4d2c866dee4d8cdc25f252d6
nicoramosr <nicorico1223@gmail.com>
2025-10-15 21:52:35 -0600

    Refactor getPrimeFactors function
    Refactor prime factorization logic to simplify checks and remove unnecessary comments.

--- Portfolio/05-JS/js/prime.js
+++ Portfolio/05-JS/js/prime.js
@@ -1,30 +1,22 @@
-/*
-    Prime Factorization - Have the user enter a number and find
-    all Prime Factors (if there are any) and display them.
-*/
-
 var getPrimeFactors = function (n) {
   "use strict";
 
   function isPrime(n) {
     var i;
-
+    if (n <= 1) return false;
     for (i = 2; i <= Math.sqrt(n); i++) {
-      if (n % i === 0) {
-        return false;
-      }
+      if (n % i === 0) return false;
     }
     return true;
   }
 
-  var i,
-    sequence = [];
-
-  //TODO: Check which numbers are factors of n and also check if
-  // that number also happens to be a prime
-
-  return sequence;
-};
-
+  var sequence = [];
+  for (let i = 2; i <= n; i++) {
+    if (n % i === 0 && isPrime(i)) {
+        sequence.push(i);
+    }
+    };
+    return sequence;
+}
 // the prime factors for this number are: [ 2, 3, 5, 7, 11, 13 ]
 console.log(getPrimeFactors(30030));
